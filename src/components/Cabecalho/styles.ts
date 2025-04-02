d420f22b6a695d8a3004956e6bf880f8b9282975
richard-piovezan <contato.piovezan@gmail.com>
2025-04-02 20:12:43 -0300

    fix all css styles to styled-components

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Cabecalho = styled.header`
+  background-color: #f9f5e7;
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
